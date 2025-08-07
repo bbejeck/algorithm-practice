4f8dd51f260ddf8fc202e1a7e58acc152d73878b
bbejeck <bbejeck@gmail.com>
2025-08-07 12:36:22 -0400

    Refactor `TwoSum` implementation for clarity and efficiency
    Redesigned the `twoSum` method to simplify logic and improve readability by removing unnecessary variables and using an early return for solutions. Added a fallback to return an empty array when no solution is found.

--- src/main/java/dev/bbejeck/arrays/TwoSum.java
+++ src/main/java/dev/bbejeck/arrays/TwoSum.java
@@ -11,21 +11,13 @@ import java.util.Map;
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> numMap = new HashMap<>();
-        int[] solution = new int[2];
-        if (nums.length == 2) {
-            solution[1] = 1;
-        } else {
-            for (int i = 0; i < nums.length; i++) {
-                int x = target - nums[i];
-                if (numMap.containsKey(x)) {
-                    solution[0] = i;
-                    solution[1] = numMap.get(x);
-                    break;
-                } else {
-                    numMap.put(nums[i], i);
-                }                                               
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
             }
+            numMap.put(nums[i], i);
         }
-        return solution;
+        return new int[0]; // No solution found
     }
 }
